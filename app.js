c915b896e88876b6d4d2b06711524074a29e492c
Your Name <christophertruongm@gmail.com>
2019-09-16 20:42:19 -0400

    added env files to hide refreshed api keys as well as mapbox for geocoding

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+const dotenv = require('dotenv').config();
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/1486ac271af238eab2a4586d6523c695/37.8267,-122.4233'
+const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/37.8267,-122.4233`
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.currently)
+    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
+})
+
+request({url: `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_KEY}&limit=1`, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude);
 })
